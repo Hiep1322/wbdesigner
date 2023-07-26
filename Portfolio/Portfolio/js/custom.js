16bfa96281f47b82e84dc44cccc29877ea5d25cb
Hiep1322 <140586564+Hiep1322@users.noreply.github.com>
2023-07-26 14:07:52 +0700

    Add files via upload

---
+++ Portfolio/Portfolio/js/custom.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+   strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker","Typography"],
+   typeSpeed:20,
+   backSpeed:20,
+   loop:true,
+ });
+
+$('.counter').countUp();
---
+++ Portfolio/Portfolio/js/tab.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+function openCity1(evt, cityName) {
+  var i, tabcontent1, tablinks1;
+  tabcontent = document.getElementsByClassName("tabcontent1");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks1");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen1").click();
